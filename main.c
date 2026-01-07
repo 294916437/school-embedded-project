2a816694bb02dd09ecb4f2bd8bba2371448e4f13
Theoyu Du <du2959346375@outlook.com>
2026-01-07 14:31:46 +0800

    feat(progess-13): complete the progress of the document 13-LVGL控件使用

--- main.c
+++ main.c
@@ -7,10 +7,31 @@
 #include <time.h>
 #include <sys/time.h>
 
-
 #define DISP_BUF_SIZE (128 * 1024)
+uint32_t custom_tick_get(void)
+{
+    static uint64_t start_ms = 0;
+    if(start_ms == 0) {
+        struct timeval tv_start;
+        gettimeofday(&tv_start, NULL);
+        start_ms = (tv_start.tv_sec * 1000) + (tv_start.tv_usec / 1000);
+    }
 
-int main(void)
+    struct timeval tv_now;
+    gettimeofday(&tv_now, NULL);
+    uint64_t now_ms;
+    now_ms = (tv_now.tv_sec * 1000) + (tv_now.tv_usec / 1000);
+
+    uint32_t time_ms = now_ms - start_ms;
+    return time_ms;
+}
+/*
+*   MODE     :       函数工程初始化LVGL函数且设置屏幕分辨率宽高
+*   package1 ：      显示屏分辨率宽度
+*   package2 :       显示屏分辨率高度
+*/
+
+void init_lvgl(int LCD_WIDTH, int LCD_HEIGHT)
 {
     /*LittlevGL init*/
     lv_init();
@@ -30,8 +51,8 @@ int main(void)
     lv_disp_drv_init(&disp_drv);
     disp_drv.draw_buf   = &disp_buf;
     disp_drv.flush_cb   = fbdev_flush;
-    disp_drv.hor_res    = 1024;
-    disp_drv.ver_res    = 600;
+    disp_drv.hor_res    = LCD_WIDTH;
+    disp_drv.ver_res    = LCD_HEIGHT;
     lv_disp_drv_register(&disp_drv);
 
     evdev_init();
@@ -43,7 +64,7 @@ int main(void)
     indev_drv_1.read_cb = evdev_read;
     lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
 
-
+#if 0
     /*Set a cursor for the mouse*/
     LV_IMG_DECLARE(mouse_cursor_icon)
     lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
@@ -53,6 +74,43 @@ int main(void)
 
     /*Create a Demo*/
     lv_demo_widgets();
+#endif
+}
+
+int main(void)
+{
+    // 1.初始化LVGL功能模块
+    init_lvgl(1024, 600);
+
+    //2、创建LCD屏幕对象
+    #if 0
+        lv_obj_t * Screen = lv_scr_act();            //创建屏幕
+        lv_obj_t * div = lv_obj_create(Screen);      //创建屏幕对象
+    #elif 1
+        lv_obj_t *div = lv_obj_create(lv_scr_act());    //创建屏幕对象
+    #endif
+    //3、设置对象大小（480*480）
+    lv_obj_set_size(div, 480, 480);
+    //4、设置对象样式（颜色、弧度、边框、背景.....）style ——>样式 R G B
+    lv_obj_set_style_bg_color(div, lv_color_hex(0xffff00), NULL);//设置对象背景颜色
+    lv_obj_set_style_border_width(div, 5, NULL);                //设置对象边框宽度
+    lv_obj_set_style_border_color(div, lv_color_hex(0x0000ff), NULL);//设置对象边框颜色
+    lv_obj_set_style_radius(div, 240, NULL);//设置对象弧度
+    //5、设置对象位置（中心）
+    lv_obj_center(div);
+    //lv_obj_align(div, LV_ALIGN_CENTER, 0, 0);
+
+    //1、创建对象按钮
+    lv_obj_t * btn = lv_btn_create(div);
+    //2、设置按钮大小(80*80)
+    lv_obj_set_size(btn, 80, 80);
+    //3、设置按钮样式 A R G B 
+    lv_obj_set_style_bg_color(btn, lv_color_hex(0x8000ff00), NULL);
+    //4、设置按钮背景透明度
+    lv_obj_set_style_bg_opa(btn, LV_OPA_80, NULL);
+    //5、设置按钮位置
+    lv_obj_center(btn);
+
 
     /*Handle LitlevGL tasks (tickless mode)*/
     while(1) {
@@ -63,21 +121,3 @@ int main(void)
     return 0;
 }
 
-/*Set in lv_conf.h as `LV_TICK_CUSTOM_SYS_TIME_EXPR`*/
-uint32_t custom_tick_get(void)
-{
-    static uint64_t start_ms = 0;
-    if(start_ms == 0) {
-        struct timeval tv_start;
-        gettimeofday(&tv_start, NULL);
-        start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
-    }
-
-    struct timeval tv_now;
-    gettimeofday(&tv_now, NULL);
-    uint64_t now_ms;
-    now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
-
-    uint32_t time_ms = now_ms - start_ms;
-    return time_ms;
-}
