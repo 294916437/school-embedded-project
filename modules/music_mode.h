370f100942d49bd7aaa6665df851cc832e147472
Theoyu Du <du2959346375@outlook.com>
2026-01-12 16:33:30 +0800

    feat(music-mode): initialize the music module

---
+++ modules/music_mode.h
@@ -0,0 +1,14 @@
+#ifndef _MUSIC_MODE_H
+#define _MUSIC_MODE_H
+#include <stdio.h>
+#include <unistd.h>
+
+void play_music(void);
+void stop_music(void);
+void next_music(void);
+void previous_music(void);
+void continue_music_mode(void);
+void end_music_mode(void);
+
+
+#endif
